64e3c7135c2dd2ab0afa9b4377c702e371c80c80
masahitoooki <58727760+masahitoooki@users.noreply.github.com>
2021-01-29 10:34:57 +0900

    initial

---
+++ demo/DemoController.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+/**
+ * コントローラクラス
+ * 「@SessionAttributes(types = DemoForm.class)」により、
+ * 生成したFormオブジェクトをセッションとしてもたせている
+ */
+@Controller
+@SessionAttributes(types = DemoForm.class)
+public class DemoController {
+
+    /**
+     * Formオブジェクトを初期化して返却する
+     * @return Formオブジェクト
+     */
+    @ModelAttribute("demoForm")
+    public DemoForm createDemoForm(){
+        DemoForm demoForm = new DemoForm();
+        //名前・性別の初期値を設定する
+        demoForm.setName("テスト");
+        demoForm.setSex("1");
+        return demoForm;
+    }
+
+    /**
+     * 入力画面に遷移する
+     * @param demoForm Formオブジェクト
+     * @return 入力画面へのパス
+     */
+    @GetMapping("/")
+    public String index(DemoForm demoForm){
+        return "input";
+    }
+
+    /**
+     * 確認画面に遷移する
+     * @param demoForm Formオブジェクト
+     * @return 確認画面へのパス
+     */
+    @PostMapping("/confirm")
+    public String confirm(DemoForm demoForm){
+        return "confirm";
+    }
+
+    /**
+     * 完了画面へのリダイレクトパスに遷移する
+     * @return 完了画面へのリダイレクトパス
+     */
+    @PostMapping("/send")
+    public String send(){
+        return "redirect:/complete";
+    }
+
+    /**
+     * 完了画面に遷移する
+     * @param sessionStatus セッションステータス
+     * @return 完了画面
+     */
+    @GetMapping("/complete")
+    public String complete(SessionStatus sessionStatus){
+        //セッションオブジェクトを破棄
+        sessionStatus.setComplete();
+        return "complete";
+    }
+}
